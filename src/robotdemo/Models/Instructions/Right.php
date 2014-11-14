f39f815c60ac6d8edacf42bb74519aef740e0c61
Stefan Carlton <stefan.carlton@greenbox-group.com>
2014-11-14 12:20:35 +1100

    missing formatted code

--- src/robotdemo/Models/Instructions/Right.php
+++ src/robotdemo/Models/Instructions/Right.php
@@ -5,19 +5,19 @@ namespace robotdemo\Models\Instructions;
 use robotdemo\Interfaces\InstructionInterface;
 use robotdemo\Interfaces\ObjectInterface;
 use robotdemo\Interfaces\EnvironmentInterface;
-
 use robotdemo\Models\Instructions\RotateAbstract;
 
 class Right extends RotateAbstract implements InstructionInterface
 {
-	/**
-	 * 
-	 * @param \robotdemo\Interfaces\ObjectInterface $object
-	 * @param \robotdemo\Interfaces\EnvironmentInterface $environment
-	 */
-    public function actionInstruction(ObjectInterface $object, EnvironmentInterface $environment)
-    {
-        $object->setHorizontalDirection( $this->nextHorizontalDirection( $object->getHorizontalDirection() ) );
-    }
-}
 
+  /**
+   * 
+   * @param \robotdemo\Interfaces\ObjectInterface $object
+   * @param \robotdemo\Interfaces\EnvironmentInterface $environment
+   */
+  public function actionInstruction(ObjectInterface $object, EnvironmentInterface $environment)
+  {
+    $object->setHorizontalDirection( $this->nextHorizontalDirection( $object->getHorizontalDirection() ) );
+  }
+
+}
